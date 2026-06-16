7f532bdc21a1f0685b63027582ba76689c3f8a98
Ravindu Layanga Premasiri <115610430+RavinduLayanga@users.noreply.github.com>
2026-06-16 15:17:12 +0530

    using for loop

---
+++ JavaScript/micro_pro/Data_Transformer.js
@@ -0,0 +1,33 @@
+// Client Brief 1: The Dispatcher Dashboard (Data Transformer)
+// Message from the Client (Operations Manager):
+// "We are building a new dashboard for our fleet dispatchers. Our legacy database gives us a messy list of all drivers, but the dispatchers only want to see the active ones. Also, they don't want to see all the background data—just a clean, readable label with the driver's name and what vehicle they are driving so they can assign routes quickly."
+
+// The Problem: You have raw, bloated data, and you need to filter out the noise and reshape it for a UI.
+
+// Your Input (Mock Data):
+
+// JavaScript
+// const drivers = [
+//   { id: 1, name: "Kamal", vehicle: "Van", status: "active" },
+//   { id: 2, name: "Ruwan", vehicle: "Truck", status: "off-duty" },
+//   { id: 3, name: "Nimal", vehicle: "Motorcycle", status: "active" }
+// ];
+// Acceptance Criteria: Write a script that evaluates the array and outputs a brand new array of strings that looks exactly like this:
+// ["Kamal - Van", "Nimal - Motorcycle"]
+
+console.log("Dispatcher Dashboard - Data Transformer");
+const drivers = [
+  { id: 1, name: "Kamal", vehicle: "Van", status: "active" },
+  { id: 2, name: "Ruwan", vehicle: "Truck", status: "off-duty" },
+  { id: 3, name: "Nimal", vehicle: "Motorcycle", status: "active" },
+];
+
+const activeDrivers = [];
+
+for (let i = 0; i < drivers.length; i++) {
+  if (drivers[i].status === "active") {
+    activeDrivers.push(`${drivers[i].name} - ${drivers[i].vehicle}`);
+  }
+}
+
+console.log(activeDrivers);
