3d291fb2d64ebb9bbc7d599ea30342165f0c9119
Ravindu Layanga Premasiri <115610430+RavinduLayanga@users.noreply.github.com>
2026-06-20 21:00:15 +0530

    implement book deletion functionality in UI class

--- JavaScript/book list app/app.js
+++ JavaScript/book list app/app.js
@@ -43,6 +43,12 @@ class UI {
     list.appendChild(row);
   }
 
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
   static clearFields() {
     document.querySelector("#title").value = " ";
     document.querySelector("#author").value = " ";
@@ -74,3 +80,6 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
 });
 
 // Event: remove a book
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  UI.deleteBook(e.target);
+});
