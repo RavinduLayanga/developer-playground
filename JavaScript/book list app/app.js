50e3a16bd3efff9487ee3c1feee124e9dba0640d
Ravindu Layanga Premasiri <115610430+RavinduLayanga@users.noreply.github.com>
2026-06-20 21:22:25 +0530

    add alert functionality for book addition and removal

--- JavaScript/book list app/app.js
+++ JavaScript/book list app/app.js
@@ -49,6 +49,18 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+    // vanish in 4 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 4000);
+  }
+
   static clearFields() {
     document.querySelector("#title").value = " ";
     document.querySelector("#author").value = " ";
@@ -70,16 +82,27 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  const book = new Book(title, author, isbn);
+  //validate
+  if (title === "" || author === " " || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    const book = new Book(title, author, isbn);
 
-  // Add book to ui
-  UI.addBookToList(book);
+    // Add book to ui
+    UI.addBookToList(book);
 
-  // clear field
-  UI.clearFields();
+    // Success message
+    UI.showAlert("Book Added", "sucess");
+
+    // clear field
+    UI.clearFields();
+  }
 });
 
 // Event: remove a book
 document.querySelector("#book-list").addEventListener("click", (e) => {
   UI.deleteBook(e.target);
+
+  // Sucess message
+  UI.showAlert("Book Removed", "success");
 });
