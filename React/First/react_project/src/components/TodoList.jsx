bd0ad7fd5a745824453f97eb13a9ac8a2e8a61b5
Ravindu Layanga Premasiri <115610430+RavinduLayanga@users.noreply.github.com>
2026-07-30 20:43:06 +0530

    feat: add CarList and TodoList components with respective item rendering

---
+++ React/First/react_project/src/components/TodoList.jsx
@@ -0,0 +1,19 @@
+import TodoItem from "./TodoItem";
+const TodoList = () => {
+  const listStyle = {
+    listStyleType: "none",
+    padding: 0,
+  };
+  return (
+    <div>
+      <h2>My To-Do List</h2>
+      <ul style={listStyle}>
+        <TodoItem description="Buy groceries" finished={false} />
+        <TodoItem description="Wash the car" finished={true} />
+        <TodoItem description="Reed the book" finished={false} />
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
