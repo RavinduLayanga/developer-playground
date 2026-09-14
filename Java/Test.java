291601548077a0972d4bc470ad40b472f1b78a91
Ravindu Layanga Premasiri <115610430+RavinduLayanga@users.noreply.github.com>
2026-09-14 20:37:15 +0530

    feat: add Test class with main method for string manipulation and integer increment demonstration

---
+++ Java/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        String a = "hello";
+        a.toUpperCase();
+        System.out.println(a);
+        a = a.toUpperCase();
+        // a.substring(int );
+        System.out.println(a);
+
+        int x = 5;
+        int y = x++;
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
